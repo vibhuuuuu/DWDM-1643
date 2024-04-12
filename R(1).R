1e16b1af57bf67a25e6ad7093b2edd9e0b618564
vibes <132731291+vibhuuuuu@users.noreply.github.com>
2024-04-12 18:07:12 +0530

    Add files via upload

---
+++ R(1).R
@@ -0,0 +1,4 @@
+
+num2=as.integer(readline(prompt = "enter the second number:"))
+num3=num1+num2
+print(num3)
---
+++ R(4).R
@@ -0,0 +1,7 @@
+num1=as.integer(readline(prompt = "enter the first number:"))
+num2=as.integer(readline(prompt = "enter the second number:"))
+
+num1=16
+num2=2
+num3=num1/num2
+print(num3)
